db7a16d723ade2622ca3f6219bdbd85099ca1721
Mack <mangalaxy433@gmail.com>
2023-10-18 23:32:03 +0300

    refactoring + appended normal getting events in current hour and current date

---
+++ app/backend/internal/buisness_logic/__init__.py
@@ -0,0 +1,7 @@
+from .getting_events import (get_events_by_hour,
+                            get_event_by_current_time,
+                            get_events_by_name,
+                            get_events,
+                            get_events_by_date,
+                            get_datetime_config,
+                            )
