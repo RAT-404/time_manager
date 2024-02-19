fa97331f0514002da3c3f101279198c379109b18
Mack <mangalaxy433@gmail.com>
2024-02-19 21:38:56 +0300

    appended REDIS_URL to config file

--- app/backend/internal/db/config.py
+++ app/backend/internal/db/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings
-from pydantic import PostgresDsn, field_validator
+from pydantic import PostgresDsn, field_validator, RedisDsn
 from functools import lru_cache
 
-
+    
 class Settings(BaseSettings):
     POSTGRES_DB: str
     POSTGRES_USER : str
@@ -10,10 +10,14 @@ class Settings(BaseSettings):
     POSTGRES_PORT : int
     POSTGRES_HOST : str
 
+    REDIS_PORT : int
+    REDIS_HOST : str
+
     BOT_TOKEN : str
     API_URL : str
 
     SQLALCHEMY_URL: PostgresDsn | None = None
+    REDIS_URL: RedisDsn | None = None
 
     @field_validator('SQLALCHEMY_URL')
     def get_sqlalchemy_url(cls, v, data):
@@ -30,6 +34,18 @@ class Settings(BaseSettings):
             path=f"{data.get('POSTGRES_DB')}"
         )
     
+    @field_validator('REDIS_URL')
+    def get_redis_url(cls, v, data):
+        if isinstance(v, str):
+            return v
+        
+        data = data.data
+        return RedisDsn.build(
+            scheme='redis',
+            host=data.get('REDIS_HOST'),
+            port=data.get('REDIS_PORT'),
+        )
+
 
 @lru_cache
 def get_settings():
@@ -38,3 +54,4 @@ def get_settings():
 
 if __name__ == '__main__':
     print(str(get_settings().SQLALCHEMY_URL))
+    print(str(get_settings().REDIS_URL))
